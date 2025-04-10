62078881a598039e5877162901bced8fbdb3c9c0
dgarg7599 <gargdivyansh7599@gmail.com>
2025-04-11 00:47:04 +0530

    [Divyansh Garg] ADD: Converting a Java object into JSON.

--- src/main/java/com/bridgelabz/practiceproblems/convertobject/ObjectToJson.java
+++ src/main/java/com/bridgelabz/practiceproblems/convertobject/ObjectToJson.java
@@ -1,15 +1,15 @@
 package com.bridgelabz.practiceproblems.convertobject;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.File;
 
 public class ObjectToJson {
     public static void main(String[] args) {
         try {
             Car car = new Car("Tesla", "Model X", 2024);
             ObjectMapper mapper = new ObjectMapper();
-            String json = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(car);
-            System.out.println("JSON String: ");
-            System.out.println(json);
+            mapper.writerWithDefaultPrettyPrinter().writeValue(new File("src/main/java/com/bridgelabz/practiceproblems/convertobject/car.json"), car);
+            System.out.println("JSON File created successfully.");
         }catch (Exception e){
             e.printStackTrace();
         }
